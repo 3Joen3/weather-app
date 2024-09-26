6a41476b88e89f1f89fcf122c11efb0b4b49ee24
3Joen3 <joalevall@gmail.com>
2024-09-26 17:54:19 +0200

    dotenv, url+key on homepage

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {API_KEY} from "@env";
 
 export default function App() {
+  const url = `https://api.openweathermap.org/data/2.5/forecast?lat=35&lon=139&appid=${API_KEY}`;
+  console.log(url);
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>{url}</Text>
       <StatusBar style="auto" />
     </View>
   );
