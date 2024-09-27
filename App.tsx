f8f8a83ad890c899bbb5f797d8ac23470eea30a5
JohannesAlevall <01johale@skola.boras.se>
2024-09-27 16:30:33 +0200

    Can get users position

--- App.tsx
+++ App.tsx
@@ -1,10 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import {API_KEY} from "@env";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { API_KEY } from "@env";
+import * as Location from "expo-location";
+import { useEffect, useState } from "react";
+import React from "react";
 
 export default function App() {
   const url = `https://api.openweathermap.org/data/2.5/forecast?lat=35&lon=139&appid=${API_KEY}`;
-  console.log(url);
+
+  const [location, setLocation] = useState<Location.LocationObject | null>(
+    null
+  );
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        return;
+      }
+      setLocation(await Location.getCurrentPositionAsync({}));
+    })();
+  }, []);
+
+  useEffect(() => {
+    console.log(location);
+  }, [location]);
+
   return (
     <View style={styles.container}>
       <Text>{url}</Text>
@@ -16,8 +37,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
