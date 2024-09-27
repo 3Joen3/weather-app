816e8ec4e687d60a4efb61a27d44ca4d8608aeda
3Joen3 <joalevall@gmail.com>
2024-09-27 19:39:55 +0200

    UseLocation

--- App.tsx
+++ App.tsx
@@ -1,34 +1,17 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { API_KEY } from "@env";
-import * as Location from "expo-location";
 import { useEffect, useState } from "react";
 import React from "react";
+import { useLocation } from "./useLocation";
 
 export default function App() {
+  const location = useLocation();
   const url = `https://api.openweathermap.org/data/2.5/forecast?lat=35&lon=139&appid=${API_KEY}`;
-
-  const [location, setLocation] = useState<Location.LocationObject | null>(
-    null
-  );
-
-  useEffect(() => {
-    (async () => {
-      let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== "granted") {
-        return;
-      }
-      setLocation(await Location.getCurrentPositionAsync({}));
-    })();
-  }, []);
-
-  useEffect(() => {
-    console.log(location);
-  }, [location]);
 
   return (
     <View style={styles.container}>
-      <Text>{url}</Text>
+      {location ? <Text>alt{location.coords.altitude}</Text>: <Text>Hämtar location</Text>}
       <StatusBar style="auto" />
     </View>
   );
---
+++ useLocation.ts
@@ -0,0 +1,22 @@
+import { useState, useEffect } from "react";
+import * as Location from "expo-location";
+
+export function useLocation() {
+  const [location, setLocation] = useState<Location.LocationObject | null>(
+    null
+  );
+
+  useEffect(() => {
+    async function getPosition() {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        console.log("hej");
+        return null;
+      }
+      setLocation(await Location.getCurrentPositionAsync());
+    }
+    getPosition();
+  }, []);
+
+  return location;
+}
