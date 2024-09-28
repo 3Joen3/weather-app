915db263ec0a0bf5dae36e8aa6a1fb9f09d2e844
JohannesAlevall <01johale@skola.boras.se>
2024-09-28 15:35:25 +0200

    Update IWeatherData

--- types.ts
+++ types.ts
@@ -1,20 +1,21 @@
 export interface IWeatherData {
-    city: {
-      name: string;
+  city: {
+    name: string;
+  };
+  list: {
+    dt: number;
+    main: {
+      temp: number;
+      feels_like: number;
+      temp_min: number;
+      temp_max: number;
     };
-    list: {
-      main: {
-        temp: number;
-        feels_like: number;
-        temp_min: number;
-        temp_max: number;
-      };
-      weather: {
-        id: number;
-        main: string;
-        description: string;
-        icon: string;
-      };
-      pop: number;
+    weather: {
+      id: number;
+      main: string;
+      description: string;
+      icon: string;
     }[];
-  }
+    pop: number;
+  }[];
+}
