c9b8d03686aa68426fc9e2e94e508502e491be5f
3Joen3 <joalevall@gmail.com>
2024-09-30 21:22:12 +0200

    Error on empty search

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -4,6 +4,7 @@ import {
   TextInput,
   StyleSheet,
   TouchableOpacity,
+  Text,
 } from "react-native";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 
@@ -12,34 +13,46 @@ interface Props {
   onSubmit: (searchTerm: string) => Promise<void>;
 }
 
-export default function SearchBar({
-  placeholder,
-  onSubmit,
-}: Props) {
-  const [text, onChangeText] = useState("");
+export default function SearchBar({ placeholder, onSubmit }: Props) {
+  const [inputValue, setInputValue] = useState("");
+  const [error, setError] = useState("");
+
+  async function handleSubmit() {
+    if (!inputValue) {
+      setError("Du måste ange en stad.");
+      return;
+    }
+    setError("");
+    await onSubmit(inputValue);
+    setInputValue("");
+  }
 
   return (
     <View style={styles.container}>
-      <TextInput
-        style={styles.input}
-        placeholder={placeholder}
-        placeholderTextColor={"black"}
-        onChangeText={onChangeText}
-        onSubmitEditing={() => onSubmit(text)}
-        value={text}
-      />
-      <TouchableOpacity
-        onPress={() => onSubmit(text)}
-        style={styles.button}
-      >
-        <FontAwesome name="search" size={24} color={"white"} />
-      </TouchableOpacity>
+      <View style={styles.barContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder={placeholder}
+          placeholderTextColor={"black"}
+          onChangeText={setInputValue}
+          onSubmitEditing={handleSubmit}
+          value={inputValue}
+        />
+        <TouchableOpacity onPress={handleSubmit} style={styles.button}>
+          <FontAwesome name="search" size={24} color={"white"} />
+        </TouchableOpacity>
+      </View>
+      {error && <Text style={styles.errorMessage}>{error}</Text>}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    width: "100%",
+    alignItems: "center"
+  },
+  barContainer: {
     borderWidth: 1,
     borderRadius: 14,
     flexDirection: "row",
@@ -52,7 +65,7 @@ const styles = StyleSheet.create({
     height: "100%",
     paddingHorizontal: 12,
     justifyContent: "center",
-    backgroundColor: "black"
+    backgroundColor: "black",
   },
   input: {
     flex: 1,
@@ -61,6 +74,10 @@ const styles = StyleSheet.create({
     height: "100%",
     backgroundColor: "white",
     borderBottomLeftRadius: 14,
-    borderTopLeftRadius: 14
+    borderTopLeftRadius: 14,
   },
+  errorMessage:{
+    color: "red",
+    marginTop: 5
+  }
 });
