fe2d120d86b79110e8893fda53cf774ca3de7442
3Joen3 <joalevall@gmail.com>
2024-10-01 01:03:54 +0200

    Expo router

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Tabs } from 'expo-router'
+import AntDesign from '@expo/vector-icons/AntDesign';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+
+export default function _layout() {
+  return (
+    <Tabs>
+        <Tabs.Screen name='index' options={{tabBarLabel: "Home", headerTitle: "Home", tabBarIcon: ({ color }) => <AntDesign name="home" size={24} color="black" />, headerShown: false}}/>
+        <Tabs.Screen name='forecasts' options={{tabBarLabel: "Forecasts", headerTitle: "Forecasts", tabBarIcon: ({ color }) => <MaterialCommunityIcons name="weather-sunset" size={24} color="black" />, headerShown: false}}/>
+    </Tabs>
+  )
+}
