eb8c5a804376a4eb703e1ebad24f210c44b91d64
3Joen3 <joalevall@gmail.com>
2024-09-30 20:15:42 +0200

    useWeather maps data

--- hooks/useWeather.ts
+++ hooks/useWeather.ts
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { IWeatherData } from "../types";
+import { IOpenWeatherResponse, IWeatherData } from "../types";
 import { API_KEY } from "@env";
 
 export function useWeather(latitude: number | null, longitude: number | null) {
@@ -9,15 +9,29 @@ export function useWeather(latitude: number | null, longitude: number | null) {
     (async () => {
       if (latitude && longitude) {
         const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`;
-        setWeatherData(await fetch(url).then((response) => response.json()));
+        const data = await fetch(url).then((response) => response.json());
+        setWeatherData(mapData(data));
       }
     })();
   }, [latitude, longitude]);
 
   async function fetchWeatherDataByCity(city: string) {
     const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=fe215cac13d63320405264414e4fffb4`;
-    setWeatherData(await fetch(url).then((response) => response.json()));
+    const data = await fetch(url).then((response) => response.json());
+    setWeatherData(mapData(data));
   }
 
   return { weatherData, fetchWeatherDataByCity };
+}
+
+function mapData(apiCall: IOpenWeatherResponse): IWeatherData
+{
+  return {
+    city: apiCall.city.name,
+    forecasts: apiCall.list.map((apiForecast) => ({
+      time: new Date(apiForecast.dt * 1000),
+      degreesCelsius: Math.round((apiForecast.main.temp - 273.15) / 0.5)*0.5,
+      iconUrl: apiForecast.weather[0].icon
+    }))
+  }
 }
