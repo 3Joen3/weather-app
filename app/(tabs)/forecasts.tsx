8b1608d9d6000cf06f71dc53f695c99e9bd1e0ef
JohannesAlevall <01johale@skola.boras.se>
2024-10-01 19:01:10 +0200

    Gradient background for screens

--- app/(tabs)/forecasts.tsx
+++ app/(tabs)/forecasts.tsx
@@ -1,8 +1,24 @@
-import React from 'react'
-import { Text } from 'react-native'
+import { LinearGradient } from "expo-linear-gradient";
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
 
-export default function forecasts() {
+export default function Forecasts() {
   return (
-    <Text>ForeCasts</Text>
-  )
+    <LinearGradient
+      style={StyleSheet.absoluteFill}
+      colors={["#24C6DC", "#514A9D"]}
+    >
+      <View style={styles.container}>
+        <Text>Forecasts</Text>
+      </View>
+    </LinearGradient>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,8 +1,24 @@
-import React from 'react'
-import { Text } from 'react-native'
+import { LinearGradient } from "expo-linear-gradient";
+import React from "react";
+import { View, StyleSheet, Text } from "react-native";
 
 export default function Home() {
   return (
-    <Text>Hi</Text>
-  )
+    <LinearGradient
+      colors={["#24C6DC", "#514A9D"]}
+      style={StyleSheet.absoluteFill}
+    >
+      <View style={styles.container}>
+        <Text>Home</Text>
+      </View>
+    </LinearGradient>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
